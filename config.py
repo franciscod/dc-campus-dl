61b3f7bde8899f91a0738af626effdd990386ea3
Francisco Demartino <demartino.francisco@gmail.com>
2018-03-19 14:22:31 -0300

    config 2018-c1

--- config.py
+++ config.py
@@ -1,10 +1,32 @@
 # See LICENSE file for copyright and license details.
 
 """
-(nombre, id en el campus),
 Por ejemplo, el link a PLP verano 2018 es:
 https://campus.exactas.uba.ar/course/view.php?id=1059
 y el numero del final (1059) es el id.
+
+[(nombre, id en el campus)]
 """
-MATERIAS = (
-)
+MATERIAS = [
+  ("algo1-m", 987),
+  ("algo1-v", 991),
+  ("algo2-m", 989),
+  ("algo2-v", 990),
+  ("algo3", 992),
+  ("aap", 1017),
+  ("acd", 1025),
+  ("arqs-web", 1060),
+  ("azar", 1026),
+  ("bases", 1001),
+  ("ing1", 1039),
+  ("ing2", 1003),
+  ("ibc", 1027),
+  ("intro-comp", 1010),
+  ("intro-robot", 1028),
+  ("lyc", 994),
+  ("metodos", 994),
+  ("orga1-m", 1100),
+  ("orga1-v", 997),
+  ("orga2", 998),
+  ("plp", 995),
+]
